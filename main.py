d735eb794d85237cb4446e9fac122750c239d3f0
bishaldebroy007 <bishaldebroy2000@gmail.com>
2026-04-12 16:54:57 +0600

    add: new path

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ app = FastAPI()
 # Decorator
 
 @app.get("/") ## root directories
-
 def read_root():
     return {"Python": "API"}
 
+
+@app.get("/aiquest")
+def aiquest():
+    return {"AI": "QUEST"}
 
