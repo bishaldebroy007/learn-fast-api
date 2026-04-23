a1587ddb79cd9543f97fc1ef79a4b1262a0b23a3
bishaldebroy007 <bishaldebroy2000@gmail.com>
2026-04-23 15:35:42 +0600

    add: json loader/helper function

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import json
 
 app = FastAPI()
 
 # Decorator
 
+# Lets make a helper function to load data from the json file
+def load_data():
+    with open("PatientData.json", "r") as file:
+        data = json.load(file)
+        
+    return data
+
+
 @app.get("/") ## root directories
 def read_root():
-    return {"Python": "API"}
+    return {"message": "Patient Management System API"}
 
 
-@app.get("/aiquest")
-def aiquest():
-    return {"AI": "QUEST"}
+@app.get("/about")
+def about():
+    return {"message": "A fuly functional API to manage your patient records."}
 
-@app.get("/python")
-def python():
-    return {"Python": "xyz."}
-
+@app.get()
