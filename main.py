a6d05c2867210dc6bb57adf5364a8be71a700a89
bishaldebroy007 <bishaldebroy2000@gmail.com>
2026-04-23 17:04:14 +0600

    add: dynamic path patient

--- main.py
+++ main.py
@@ -31,4 +31,10 @@ def view():
 
 # Dynamic endpoint to view a specific patient record by ID
 @app.get("/patient/{patient_id}")
-
+def view_patient(patient_id: str):
+    data = load_data()
+    
+    if patient_id in data:
+        return data[patient_id]
+    else:
+        return {"error": "patient not found"}
