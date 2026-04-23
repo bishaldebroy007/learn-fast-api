30160d7aea59eda7be6bd21241d19317093ba31e
bishaldebroy007 <bishaldebroy2000@gmail.com>
2026-04-23 15:41:09 +0600

    Patient data view working and sending response

--- main.py
+++ main.py
@@ -8,12 +8,11 @@ app = FastAPI()
 
 # Lets make a helper function to load data from the json file
 def load_data():
-    with open("PatientData.json", "r") as file:
-        data = json.load(file)
-        
+    with open("patient.json", "r") as file:  # Add the file name and open in read mode.
+        data = json.load(file)     
     return data
 
-
+# Endpoints start
 @app.get("/") ## root directories
 def read_root():
     return {"message": "Patient Management System API"}
@@ -23,4 +22,9 @@ def read_root():
 def about():
     return {"message": "A fuly functional API to manage your patient records."}
 
-@app.get()
+
+@app.get("/view")
+def view():
+    data = load_data() # Loading the data from json using the helper function, for viewing purpose.
+    return data
+
