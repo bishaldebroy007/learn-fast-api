9da6e8a1ccb641aa8253d2bec4d39bfb6d635f57
bishaldebroy007 <bishaldebroy2000@gmail.com>
2026-04-25 00:25:00 +0600

    add: single patient tracking system

--- main.py
+++ main.py
@@ -33,8 +33,9 @@ def view():
 @app.get("/patient/{patient_id}")
 def view_patient(patient_id: str):
     data = load_data()
-    
-    if patient_id in data:
-        return data[patient_id]
-    else:
-        return {"error": "patient not found"}
+    patient_id = patient_id.upper() # Made it upper case for exact match
+    patient = data.get(patient_id)  # safer lookup
+    if patient:
+        return patient
+    return {"error": f"Patient {patient_id}, not found"}
+
