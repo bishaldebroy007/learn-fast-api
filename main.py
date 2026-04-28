8a4f098341adc94232153040c8c6bb7697a94aff
bishaldebroy007 <bishaldebroy2000@gmail.com>
2026-04-28 17:46:08 +0600

    add: post request

--- main.py
+++ main.py
@@ -1,22 +1,54 @@
 from fastapi import FastAPI, Path, HTTPException, Query
-from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field, computed_field
+from typing import Annotated, Literal, Optional
 import json
+# -------Imports --------
 
 app = FastAPI()
 
 class Patient(BaseModel):
-    name: str
-    city: str
-    age: str
-    gender: str
-    height: float
-    weight: float
+
+    id: Annotated[str, Field(..., description='ID of the patient', examples=['P001'])]
+    name: Annotated[str, Field(..., description='Name of the patient')]
+    city: Annotated[str, Field(..., description='City where the patient is living')]
+    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the patient')]
+    gender: Annotated[Literal['male', 'female', 'others'], Field(..., description='Gender of the patient')]
+    height: Annotated[float, Field(..., gt=0, description='Height of the patient in mtrs')]
+    weight: Annotated[float, Field(..., gt=0, description='Weight of the patient in kgs')]
+    
+    
+    ## Computed field for BMI
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = round(self.weight/(self.height**2),2)
+        return bmi
+    
+    @computed_field
+    @property
+    def verdict(self) -> str:  ## Decision making based on BMI value, to determine the health status of the patient.
+
+        if self.bmi < 18.5:
+            return 'Underweight'
+        elif self.bmi < 25:
+            return 'Normal'
+        elif self.bmi < 30:
+            return 'Overweight'
+        else:
+            return 'Obese'
 
 # Lets make a helper function to load data from the json file
 def load_data():
     with open("patient.json", "r") as file:  # Add the file name and open in read mode.
         data = json.load(file)     
     return data
+
+# Lets make another helper function to add data to the json
+def save_data(data):
+    with open("patient.json", "w") as file:
+        json.dump(data, file)
+        
 
 # Endpoints start
 @app.get("/") ## root directories
@@ -79,3 +111,22 @@ def patient_sort(sort_by: str = Query(..., description="Sort- height/weight/bmi"
     # endpoint: http://127.0.0.1:8000/sort?sort_by=height&order=desc
     
     
+## POST
+@app.post("/create")
+def creat_patient(patient: Patient): # here "patient" is a pydancic object
+    # load existing data from json in this case
+    data = load_data()
+    
+    # checking if the patient already exists
+    if patient.id in data:
+        raise HTTPException(status_code=400, detail="patient already exists")
+    
+    # Now if things are alright, add those to database: add patient to pydantic object(patient)
+    else:
+        data[patient.id] = patient.model_dump(exclude=["id"]) # Convert the object to python disctionary using, model_dump()
+        
+    # Save into the json file
+    save_data(data)
+    
+    return JSONResponse(status_code=201, content={"message": {"Patient created successfully"}})
+        
